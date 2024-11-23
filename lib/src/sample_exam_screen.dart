a031f2e8999c3f0f221fe78938287b4f783d5518
dennis <info.dennis.durmus@gmail.com>
2024-11-23 11:59:33 +0100

    Column in Padding gewrapt, Circle Avatar Image hinzugefügt, CheckboxListTitle  Widget hinzugefügt

--- lib/src/sample_exam_screen.dart
+++ lib/src/sample_exam_screen.dart
@@ -9,13 +9,33 @@ class SampleExamScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("Probe Wissenscheck 4"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: CircleAvatar(radius: 80),
-            ),
-          ],
+        body: Padding(
+          padding: EdgeInsets.only(top: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: 90,
+                  backgroundImage: AssetImage("assets/moon.png"),
+                ),
+              ),
+              CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                value: true,
+                onChanged: (value) {},
+                title: Text("Vorbereitung"),
+                subtitle: Text("Auf den Wissenscheck 4"),
+              ),
+              CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                value: true,
+                onChanged: (value) {},
+                title: Text("Durchführung"),
+                subtitle: Text("Des Wissenscheck 4"),
+              ),
+            ],
+          ),
         ));
   }
 }
