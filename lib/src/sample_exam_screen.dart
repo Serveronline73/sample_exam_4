80e6ab906063ae5c3d0e964b0c53c5c1daa54771
dennis <info.dennis.durmus@gmail.com>
2024-11-23 15:43:49 +0100

    Es wurde ein Abstand für Symbole mit _swapStart Status implementiert

--- lib/src/sample_exam_screen.dart
+++ lib/src/sample_exam_screen.dart
@@ -11,6 +11,7 @@ class _SampleExamScreenState extends State<SampleExamScreen> {
   bool isChecked1 = false;
   bool isChecked2 = false;
   bool swapStart = true;
+  bool swapEnd = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,75 +20,82 @@ class _SampleExamScreenState extends State<SampleExamScreen> {
         appBar: AppBar(
           title: Text("Probe Wissenscheck 4"),
         ),
-        body: Padding(
-          padding: EdgeInsets.only(top: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Center(
-                child: Image.asset(width: 200, height: 200, "assets/moon.png"),
-              ),
-              SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: ListTile(
-                  title: Text(
-                    "Vorbereitung",
-                    style: TextStyle(color: Colors.white),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 80),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Center(
+                  child:
+                      Image.asset(width: 200, height: 180, "assets/moon.png"),
+                ),
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20,
                   ),
-                  subtitle: Text(
-                    "Auf den Wissenscheck 4",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  leading: Checkbox(
-                    value: isChecked1,
-                    activeColor: Colors.white,
-                    onChanged: (newBool) {
-                      setState(() {
-                        isChecked1 = newBool!;
-                      });
-                    },
+                  child: ListTile(
+                    title: Text(
+                      "Vorbereitung",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      "Auf den Wissenscheck 4",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    leading: Checkbox(
+                      value: isChecked1,
+                      activeColor: Colors.white,
+                      onChanged: (newBool) {
+                        setState(() {
+                          isChecked1 = newBool!;
+                        });
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: ListTile(
-                  title: Text("Durchführung",
-                      style: TextStyle(color: Colors.white)),
-                  subtitle: Text("Des Wissenscheck 4",
-                      style: TextStyle(color: Colors.white)),
-                  leading: Checkbox(
-                    value: isChecked2,
-                    activeColor: Colors.white,
-                    onChanged: (newBool) {
-                      setState(() {
-                        isChecked2 = newBool!;
-                      });
-                    },
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: ListTile(
+                    title: Text("Durchführung",
+                        style: TextStyle(color: Colors.white)),
+                    subtitle: Text("Des Wissenscheck 4",
+                        style: TextStyle(color: Colors.white)),
+                    leading: Checkbox(
+                      value: isChecked2,
+                      activeColor: Colors.white,
+                      onChanged: (newBool) {
+                        setState(() {
+                          isChecked2 = newBool!;
+                        });
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: swapStart
-                    ? MainAxisAlignment.start
-                    : MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.favorite, color: Colors.red, size: 35),
-                  Icon(Icons.circle, color: Colors.green, size: 35),
-                  Icon(Icons.star, color: Colors.blue, size: 35),
-                ],
-              ),
-              SizedBox(height: 100),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    swapStart = !swapStart;
-                  });
-                },
-                child: Text("Swap"),
-              ),
-            ],
+                SizedBox(height: 30),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.favorite, color: Colors.red, size: 45),
+                    SizedBox(height: swapStart ? 10 : 0),
+                    Icon(Icons.circle, color: Colors.green, size: 45),
+                    SizedBox(height: swapStart ? 10 : 0),
+                    Icon(Icons.star, color: Colors.blue, size: 45),
+                  ],
+                ),
+                SizedBox(height: 180),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      swapStart = !swapStart;
+                    });
+                  },
+                  child: Text("Swap"),
+                ),
+                SizedBox(height: 30),
+              ],
+            ),
           ),
         ));
   }
